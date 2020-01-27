dad560f8d9d24d81d096d5fcbfaca22b5051a9c7
Dieter Hubau <dhubau@pivotal.io>
2020-01-27 17:49:21 +0100

    Added Kubernetes configuration files for the three applications Added Kubernetes Helm charts for RabbitMQ and Redis (both clustered) Added sample project with Spring Boot 2.3.M1 containing Kubernetes build image support

---
+++ b2b-accounts/src/main/java/io/pivotal/examples/b2b/b2baccounts/B2bAccountsApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.examples.b2b.b2baccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2bAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2bAccountsApplication.class, args);
+	}
+
+}
---
+++ b2b-accounts/src/test/java/io/pivotal/examples/b2b/b2baccounts/B2bAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pivotal.examples.b2b.b2baccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2bAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
