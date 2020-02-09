11931485bff5b77258c48bf178bc83a06ff98f9e
Dieter Hubau <dhubau@pivotal.io>
2020-02-09 20:30:43 +0100

    Added better transactions for demo and updated builders/stacks/stores

--- b2b-payments/src/main/java/io/pivotal/examples/b2b/payments/PaymentProcessingScheduler.java
+++ b2b-payments/src/main/java/io/pivotal/examples/b2b/payments/PaymentProcessingScheduler.java
@@ -1,24 +1,22 @@
 package io.pivotal.examples.b2b.payments;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Component
 public class PaymentProcessingScheduler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PaymentProcessingScheduler.class);
 
-    private static final long EVERY_TEN_MINUTES = 10 * 60 * 1000;
+    private static final long NR_OF_TRANSACTIONS = 10;
     private static final long TEN_SECONDS = 10 * 1000;
 
     private PaymentProcessor paymentProcessor;
@@ -27,20 +25,16 @@ public class PaymentProcessingScheduler {
         this.paymentProcessor = paymentProcessor;
     }
 
-    @Scheduled(initialDelay = TEN_SECONDS, fixedRate = EVERY_TEN_MINUTES)
+    @Scheduled(initialDelay = TEN_SECONDS, fixedRate = TEN_SECONDS)
     public void processPayments() throws IOException, InterruptedException {
         LOGGER.info("Processing Payments...");
+        ThreadLocalRandom random = ThreadLocalRandom.current();
 
-        Path path = Paths.get("payments.csv");
-
-        BufferedReader reader = Files.newBufferedReader(path);
-        Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(reader);
-
-        for (CSVRecord record : records) {
-            String paymentId = record.get("paymentId");
-            String originAccount = record.get("originAccount");
-            String destinationAccount = record.get("destinationAccount");
-            String amount = record.get("amount");
+        for (int i = 0; i < NR_OF_TRANSACTIONS; i++) {
+            String paymentId = "" + UUID.randomUUID();
+            String originAccount = "" + random.nextInt(1000);
+            String destinationAccount = "" + random.nextInt(1000);
+            String amount = "€" + BigDecimal.valueOf(random.nextDouble(10)).setScale(2, RoundingMode.HALF_UP);
 
             Payment payment = new Payment(null, paymentId, originAccount, destinationAccount, amount, PaymentStatus.NOT_CONFIRMED);
 
