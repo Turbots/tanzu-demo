3b42ae8a6b50ca7346d0a043ab5f579855e9fc3d
Dieter Hubau <dhubau@gmail.com>
2022-10-30 19:23:42 +0100

    Removed unused file Updated frontend

--- payment-service/src/main/java/com/vmware/tanzu/demo/payments/PaymentMetrics.java
+++ payment-service/src/main/java/com/vmware/tanzu/demo/payments/PaymentMetrics.java
@@ -1,6 +1,5 @@
 package com.vmware.tanzu.demo.payments;
 
-import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +10,5 @@ public class PaymentMetrics {
 
     public PaymentMetrics(MeterRegistry meterRegistry) {
         this.meterRegistry = meterRegistry;
-
-
     }
 }
