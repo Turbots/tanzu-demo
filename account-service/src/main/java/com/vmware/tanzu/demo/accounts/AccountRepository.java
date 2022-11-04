6f44b419b995786a12474c4e6790e535c195f2b1
Dieter Hubau <dhubau@gmail.com>
2022-11-04 12:29:35 +0100

    Go

--- account-service/src/main/java/com/vmware/tanzu/demo/accounts/AccountRepository.java
+++ account-service/src/main/java/com/vmware/tanzu/demo/accounts/AccountRepository.java
@@ -1,12 +1,13 @@
 package com.vmware.tanzu.demo.accounts;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface AccountRepository extends PagingAndSortingRepository<Account, String> {
+public interface AccountRepository extends PagingAndSortingRepository<Account, String>, CrudRepository<Account, String> {
 
     List<Account> findTop50ByOrderByBalanceDesc();
 }
