ef67ce698e0628844eb3e90cdf0cb322777c9bcb
Dieter Hubau <dhubau@gmail.com>
2022-11-04 22:31:22 +0100

    Fixed confirmation-service Removed Wavefront dependencie

--- confirmation-service/src/main/java/com/vmware/tanzu/demo/confirmation/ConfirmationApplication.java
+++ confirmation-service/src/main/java/com/vmware/tanzu/demo/confirmation/ConfirmationApplication.java
@@ -2,10 +2,8 @@ package com.vmware.tanzu.demo.confirmation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
-@EnableBinding(PaymentChannels.class)
 public class ConfirmationApplication {
 
     public static void main(String[] args) {
--- confirmation-service/src/main/java/com/vmware/tanzu/demo/confirmation/ConfirmationProcessor.java
+++ confirmation-service/src/main/java/com/vmware/tanzu/demo/confirmation/ConfirmationProcessor.java
@@ -2,24 +2,29 @@ package com.vmware.tanzu.demo.confirmation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Function;
 
 @Service
 public class ConfirmationProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfirmationProcessor.class);
 
-    @StreamListener(PaymentChannels.PAYMENTS)
-    @SendTo(PaymentChannels.CONFIRMATIONS)
-    private PaymentConfirmation confirm(Payment payment) throws InterruptedException {
-        LOGGER.info("Payment [{}] received. Sending out confirmation", payment.getPaymentId());
+    @Bean
+    public Function<Payment, PaymentConfirmation> confirm() {
+        return payment -> {
+            LOGGER.info("Payment [{}] received. Sending out confirmation", payment.getPaymentId());
 
-        Thread.sleep(ThreadLocalRandom.current().nextLong(100));
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextLong(100));
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
 
-        return new PaymentConfirmation(payment.getPaymentId());
+            return new PaymentConfirmation(payment.getPaymentId());
+        };
     }
 }
