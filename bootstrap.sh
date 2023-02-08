5ca480747f465653924faf253215888e2060e954
Dieter Hubau <dhubau@gmail.com>
2023-02-08 10:50:36 +0100

    added DB for bamboo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,15 @@
-if [ -z $2 ] ; then
+if [ $# -ne 3 ]; then
+    echo "Please provide 3 arguments"
+else
+    echo "Setting up GitOps for View Cluster $1"
+    kubectx $1
+    kapp deploy -a view-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/view-cluster/tanzu-demo.yaml -y -y
 
-    echo "Setting up GitOps for View Cluster"
-    #kubectx $2
-    #kapp deploy -a view-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/view-cluster/tanzu-demo.yaml -y -y
+    echo "Setting up GitOps for Build Cluster $2"
+    kubectx $2
+    kapp deploy -a build-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/build-cluster/tanzu-demo.yaml -y
 
-fi
-
-if [ -z $3 ] ; then
-
-    echo "Setting up GitOps for Build Cluster"
-    #kubectx $3
-    #kapp deploy -a build-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/build-cluster/tanzu-demo.yaml -y
-
-fi
-
-if [ -z $4 ] ; then
-
-    echo "Setting up GitOps for Run Cluster"
-    #kubectx $4
-    #kapp deploy -a run-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/run-cluster/tanzu-demo.yaml -y
-
+    echo "Setting up GitOps for Run Cluster $3"
+    kubectx $3
+    kapp deploy -a run-cluster-gitops -f https://raw.githubusercontent.com/Turbots/gitops/main/kapp/run-cluster/tanzu-demo.yaml -y
 fi
